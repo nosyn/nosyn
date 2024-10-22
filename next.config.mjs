2c4161cf0c16ec61ed35cb1eab73757dd705f4a7
Son Yen Nguyen <nyenson1997@gmail.com>
2024-10-22 20:30:13 +0700

    Initial project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Ensure that all imports of 'yjs' resolve to the same instance
+      config.resolve.alias['yjs'] = resolve(__dirname, 'node_modules/yjs');
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
