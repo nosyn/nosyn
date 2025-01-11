9797d1323326d601651d6b2a56b36fe47ecce286
Son Yen Nguyen <nyenson1997@gmail.com>
2025-01-11 17:10:01 +0700

    Fix build

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 function Table({ data }: { data: { headers: string[]; rows: string[][] } }) {
-  let headers = data.headers.map((header, index) => (
+  const headers = data.headers.map((header, index) => (
     <th key={index}>{header}</th>
   ));
-  let rows = data.rows.map((row, index) => (
+  const rows = data.rows.map((row, index) => (
     <tr key={index}>
       {row.map((cell, cellIndex) => (
         <td key={cellIndex}>{cell}</td>
@@ -24,26 +24,31 @@ function Table({ data }: { data: { headers: string[]; rows: string[][] } }) {
   );
 }
 
-function CustomLink(props: any) {
-  let href = props.href;
-
-  if (href.startsWith("/")) {
+function CustomLink({
+  href,
+  ...rest
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  if (href.startsWith('/')) {
     return (
-      <Link href={href} {...props}>
-        {props.children}
+      <Link href={href} {...rest}>
+        {rest.children}
       </Link>
     );
   }
 
-  if (href.startsWith("#")) {
-    return <a {...props} />;
+  if (href.startsWith('#')) {
+    return <a {...rest} />;
   }
 
-  return <a target="_blank" rel="noopener noreferrer" {...props} />;
+  return <a target='_blank' rel='noopener noreferrer' {...rest} />;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function RoundedImage(props: any) {
-  return <Image alt={props.alt} className="rounded-lg" {...props} />;
+  return <Image alt={props.alt} className='rounded-lg' {...props} />;
 }
 
 // This replaces rehype-slug
@@ -52,23 +57,23 @@ function slugify(str: string) {
     .toString()
     .toLowerCase()
     .trim() // Remove whitespace from both ends of a string
-    .replace(/\s+/g, "-") // Replace spaces with -
-    .replace(/&/g, "-and-") // Replace & with 'and'
-    .replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
-    .replace(/\-\-+/g, "-"); // Replace multiple - with single -
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
+    .replace(/\-\-+/g, '-'); // Replace multiple - with single -
 }
 
 function createHeading(level: number) {
   const Heading = ({ children }: { children: React.ReactNode }) => {
-    let slug = slugify(children as string);
+    const slug = slugify(children as string);
     return React.createElement(
       `h${level}`,
       { id: slug },
       [
-        React.createElement("a", {
+        React.createElement('a', {
           href: `#${slug}`,
           key: `link-${slug}`,
-          className: "anchor",
+          className: 'anchor',
         }),
       ],
       children
--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -1,7 +1,7 @@
 import { Icons } from '@/components/icons';
 import { HomeIcon, NotebookIcon } from 'lucide-react';
 
-export const DATA = {
+export const DATA: Resume = {
   name: 'Son Nguyen',
   initials: 'SN',
   url: 'https://nosyn.dev',
@@ -107,34 +107,5 @@ export const DATA = {
       end: '2020',
     },
   ],
-  projects: [
-    {
-      title: 'Chat Collect',
-      href: 'https://chatcollect.com',
-      dates: 'Jan 2024 - Feb 2024',
-      active: true,
-      description:
-        'With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.',
-      technologies: [
-        'Next.js',
-        'Typescript',
-        'PostgreSQL',
-        'Prisma',
-        'TailwindCSS',
-        'Stripe',
-        'Shadcn UI',
-        'Magic UI',
-      ],
-      links: [
-        {
-          type: 'Website',
-          href: 'https://chatcollect.com',
-          icon: <Icons.globe className='size-3' />,
-        },
-      ],
-      image: '',
-      video:
-        'https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4',
-    },
-  ],
+  projects: [],
 } as const;
