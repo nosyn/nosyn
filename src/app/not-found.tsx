dce3a01fc53694c426294ca244fe00004cf9a7a7
Son Yen Nguyen <nyenson1997@gmail.com>
2025-01-18 23:15:03 +0700

    Fix min h screen

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function NotFoundPage() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen p-4'>
+    <div className='flex flex-col items-center justify-center min-h-dvh p-4'>
       <div className='text-center space-y-4'>
         <h1 className='text-6xl font-bold text-primary'>404</h1>
         <h2 className='text-2xl font-semibold '>Page not found</h2>
