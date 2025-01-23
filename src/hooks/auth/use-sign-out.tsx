e371618b0d505ecde447459931b48030c4c97639
Son Yen Nguyen <nyenson1997@gmail.com>
2025-01-23 23:01:02 +0700

    Fixed Signed Out Hook

--- src/hooks/auth/use-sign-out.tsx
+++ src/hooks/auth/use-sign-out.tsx
@@ -1,8 +1,11 @@
 import { authClient } from '@/lib/auth-client';
 import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
 export const useSignOut = () => {
+  const router = useRouter();
+
   return useMutation({
     mutationKey: ['sign-out'],
     mutationFn: async () => {
@@ -10,6 +13,7 @@ export const useSignOut = () => {
     },
     onSuccess: () => {
       toast.success('Signed out ✌️', { id: 'sign-out' });
+      router.push('/sign-in');
     },
     onError: (error) => {
       toast.error(error.message, { id: 'sign-out' });
